d95de69bb8ac4fb54c9a083be966b7327b2c8479
prashanth-sams <sams.prashanth@gmail.com>
2019-09-11 14:27:09 +0400

    logger

--- src/utility/appiumdriver.py
+++ src/utility/appiumdriver.py
@@ -4,6 +4,14 @@ import sys, os
 import pdb
 from datetime import datetime
 import pytest
+import logging
+
+logging.basicConfig(
+    filename='app.log',
+    format='%(asctime)s - %(levelname)s: %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+    level=logging.INFO
+)
 
 class Driver(unittest.TestCase):
     
@@ -11,8 +19,12 @@ class Driver(unittest.TestCase):
         unittest.TestCase.__init__(self, driver)
         
     def setUp(self):
+        """
+        This method instantiates the appium driver
+        """
         Driver.cli
         
+        logging.info("Configuring desired capabilities")
         if self.app == 'ios':
             desired_caps = {}
             desired_caps['platformName'] = 'ios'
@@ -27,6 +39,7 @@ class Driver(unittest.TestCase):
             desired_caps['appPackage'] = 'com.wdiodemoapp'
             desired_caps['appActivity'] = 'com.wdiodemoapp.MainActivity'
         
+        logging.info("Initiating Appium driver")
         self.driver = webdriver.Remote("http://0.0.0.0:4723/wd/hub", desired_caps)
 
     def tearDown(self):
@@ -38,6 +51,7 @@ class Driver(unittest.TestCase):
         test_name = self._testMethodName
         for self._testMethodName, error in self._outcome.errors:
             if error:
+                logging.error("Taking screenshot on failure")
                 if not os.path.exists('screenshots'):
                     os.makedirs('screenshots')
                     
