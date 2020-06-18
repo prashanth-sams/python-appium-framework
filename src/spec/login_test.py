3c0e086870ddc4b43fc436209047df0f1391a0a4
prashanth-sams <sams.prashanth@gmail.com>
2020-06-18 13:09:53 +0400

    added implicit wait

--- src/spec/login_test.py
+++ src/spec/login_test.py
@@ -1,4 +1,5 @@
 import unittest
+from selenium.webdriver.common.by import By
 from src.utility.appiumdriver import Driver
 from src.locators.home_screen_locators import HomeLocators
 from src.locators.login_screen_locators import LoginLocators
@@ -7,9 +8,9 @@ class LoginTest(Driver):
 
     def __init__(self, driver):
         super().__init__(driver)
-        
+
     def test_login(self):
-        self.driver.find_element_by_id(HomeLocators.loginMenu).click()
-        self.driver.find_element_by_id(LoginLocators.inputField).send_keys("johnsmith@gmail.com")
-        self.driver.find_element_by_id(LoginLocators.passwordField).send_keys("password")
-        self.driver.find_element_by_id(LoginLocators.loginButton).click()
+        self.driver.find_element_by_xpath(HomeLocators.loginMenu).click()
+        self.driver.find_element(by=By.XPATH, value=LoginLocators.inputField).send_keys("johnsmith@gmail.com")
+        self.driver.find_element_by_xpath(LoginLocators.passwordField).send_keys("password")
+        self.driver.find_element_by_xpath(LoginLocators.loginButton).click()
