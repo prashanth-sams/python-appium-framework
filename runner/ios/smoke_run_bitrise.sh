1c39574f28dac8d9c524d58cdca61f0f029e1dfa
prashanth-sams <sams.prashanth@gmail.com>
2020-10-15 18:40:17 +0400

    added bitrise config

---
+++ runner/ios/smoke_run_bitrise.sh
@@ -0,0 +1,2 @@
+invoke test --env='staging' --lang='en' --app='ios' --device='bitrise'
+invoke slack --env='staging' --lang='en' --slack='off' --app='ios'
--- tasks.py
+++ tasks.py
@@ -10,7 +10,7 @@ def test(c, env='staging', lang='en', app='android', device='emulator'):
     Enable and modify this to add local storage
     """
     # os.system("adb push $(pwd)/data/<your_app_prefs_file>.xml /data/data/<packageName>/shared_prefs")
-    c.run(f'python3 -m pytest src/spec/{app}/login_test.py --app={app} --device={device}')
+    c.run(f'python3 -m pytest src/spec/{app}/*.py --app={app} --device={device}')
 
 @task
 def slack(c, env='staging', country='AE', slack='on', app='android'):
